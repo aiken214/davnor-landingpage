2f817eaa0f0e032fb7976d70f1158621d7f64864
aiken214 <paul.arsolon@deped.gov.ph>
2024-09-30 14:09:52 +0800

    asus 9/30/2024

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,11 @@
 import background from "./background.jpg";
+import davnor_logo from "./davnor_logo.png";
+import gmail_logo from "./gmail_logo.JPG";
+import m365_logo from "./m365_logo.JPG";
 import csm_logo from "./csm_logo.JPG";
 import mooe_logo from "./mooe_logo.JPG";
 import dts_logo from "./dts_logo.JPG";
+import infohub_logo from "./infohub_logo.JPG";
 import psmis_logo from "./psmis_logo.JPG";
 import procsys_logo from "./procsys_logo.JPG";
 import sds from "./reynaldo_mellorida.JPG";
@@ -12,9 +16,13 @@ import member3 from "./franklin_maynucas.JPG";
 
 export { 
     background, 
+    davnor_logo, 
+    gmail_logo, 
+    m365_logo, 
     csm_logo, 
     mooe_logo, 
     dts_logo, 
+    infohub_logo, 
     psmis_logo, 
     procsys_logo,
     sds,
